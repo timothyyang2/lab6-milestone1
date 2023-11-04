d14bf838eef3e5a7b40fa27c6a7c571b70885c24
timothyyang1 <tyang287@wisc.edu>
2023-11-04 18:31:53 -0500

    Initial commit

---
+++ app/src/main/java/com/cs407/lab6_milestone1/MainActivity.java
@@ -0,0 +1,32 @@
+package com.cs407.lab6_milestone1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity {
+
+    private final LatLng mDestinationLatLng = new LatLng(43.07575194669872, -89.40401697465877);
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_map);
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+
+                googleMap.addMarker(new MarkerOptions()
+                        .position(mDestinationLatLng)
+                        .title("Destination"));
+        });
+    }
+}
